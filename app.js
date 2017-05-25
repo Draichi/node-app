655f4c1581ee53aaabb3f55ee752b774a7d8905f
Lucas Draichi <lucasdraichi@gmail.com>
2017-05-24 21:54:18 -0300

    h

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var app = require('./config/express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http);    
 
-app.set('io', io);
+app.set('io',io);	
 
 var porta = process.env.PORT || 3000;
-http.listen(porta, function () {
-	console.log("Servidor ta Rodando em heroku");
+var server = http.listen(porta, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+
 });
+
+
