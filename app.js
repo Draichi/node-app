fbd7f1237fae4d83e22693a43920c067e382c814
Lucas Draichi <lucasdraichi@gmail.com>
2017-05-24 21:26:38 -0300

    commit antes do deploy

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var io = require('socket.io')(http);
 
 app.set('io',io);
 
-http.listen(3000, function () {
-	console.log("Servidor ta Rodando!!! esta msg vem de app.js");
+var porta = process.env.PORT || 3000;
+var server = http.listen(porta, function(){
+
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log("Servidor ta Rodando em http://%s:%s", host, port);
 });
