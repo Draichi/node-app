151c8ba8e6f2f2c65d53982245be64b6e8d72268
Lucas Draichi <lucasdraichi@gmail.com>
2017-05-24 21:40:35 -0300

    alterando package.json

--- app.js
+++ app.js
@@ -2,13 +2,9 @@ var app = require('./config/express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.set('io',io);
+app.set('io', io);
 
 var porta = process.env.PORT || 3000;
-var server = http.listen(porta, function(){
-
-	var host = server.address().address;
-	var port = server.address().port;
-
-	console.log("Servidor ta Rodando em http://%s:%s", host, port);
+http.listen(porta, function () {
+	console.log("Servidor ta Rodando em heroku");
 });
